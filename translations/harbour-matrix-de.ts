4d69bc3f2f24bf5c75a636ce32712f066696c030
Rudi Timmermans <rudi.timmer@gmx.us>
2017-11-20 15:26:41 +0100

    Add files via upload

---
+++ translations/harbour-matrix-de.ts
@@ -0,0 +1,291 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="de">
+<context>
+    <name>AboutPage</name>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="18"/>
+        <source>About Matriksi</source>
+        <translation>Über Matriksi</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="62"/>
+        <source>An unofficial Matrix Client for SailfishOS</source>
+        <translation>Ein inoffizieller Matrix-Client für SailfishOS</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="71"/>
+        <source>Matriksi is an unofficial Matrix Client for SailfishOS and distributed under the GPLv3 license.</source>
+        <translation>Matriksi ist ein inoffizieller Matrix-Client für SailfishOS und wird unter der GPLv3 vertrieben.</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="83"/>
+        <source>Developers</source>
+        <translation>Entwickler</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="91"/>
+        <source>Donate with Paypal</source>
+        <translation>Mit Paypal unterstützen</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="100"/>
+        <source>Translations</source>
+        <translation>Übersetzungen</translation>
+    </message>
+</context>
+<context>
+    <name>ChatRoom</name>
+    <message>
+        <location filename="../qml/pages/ChatRoom.qml" line="35"/>
+        <source>Say something...</source>
+        <translation>Sage etwas</translation>
+    </message>
+</context>
+<context>
+    <name>ChatRoom2</name>
+    <message>
+        <location filename="../qml/pages/ChatRoom2.qml" line="49"/>
+        <source>Message @</source>
+        <translation>Nachricht @</translation>
+    </message>
+</context>
+<context>
+    <name>CoverPage</name>
+    <message>
+        <location filename="../qml/cover/CoverPage.qml" line="38"/>
+        <source>My Cover</source>
+        <translation>Meine Titelseite</translation>
+    </message>
+</context>
+<context>
+    <name>DevelopersPage</name>
+    <message>
+        <location filename="../qml/pages/DevelopersPage.qml" line="18"/>
+        <source>Matriksi developers</source>
+        <translation>Matriksi Entwickler</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/DevelopersPage.qml" line="22"/>
+        <source>Developers</source>
+        <translation>Entwickler</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/DevelopersPage.qml" line="27"/>
+        <source>Previous developer</source>
+        <translation>Frühere Entwickler</translation>
+    </message>
+</context>
+<context>
+    <name>Login</name>
+    <message>
+        <location filename="../qml/pages/Login.qml" line="11"/>
+        <source>Please wait...</source>
+        <translation>Bitte warten...</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Login.qml" line="26"/>
+        <location filename="../qml/pages/Login.qml" line="79"/>
+        <source>Matriksi</source>
+        <translation>Matriksi</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Login.qml" line="87"/>
+        <source>User Name or Matrix ID:</source>
+        <translation>Benutzername oder Matrix ID:</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Login.qml" line="88"/>
+        <source>username[:server][:port]</source>
+        <translation>username[:server][:port]</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Login.qml" line="96"/>
+        <source>Password:</source>
+        <translation>Passwort:</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Login.qml" line="101"/>
+        <source>Login</source>
+        <translation>Login</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Login.qml" line="109"/>
+        <source>Create an Matrix account</source>
+        <translation>Einen Matrix Account erstellen</translation>
+    </message>
+</context>
+<context>
+    <name>MessageEventModel</name>
+    <message>
+        <location filename="../src/models/messageeventmodel.cpp" line="217"/>
+        <source>invited %1 to the room</source>
+        <translation>%1 in den Raum eingeladen</translation>
+    </message>
+    <message>
+        <location filename="../src/models/messageeventmodel.cpp" line="218"/>
+        <source>joined the room</source>
+        <translation>hat den Raum betreten</translation>
+    </message>
+    <message>
+        <location filename="../src/models/messageeventmodel.cpp" line="224"/>
+        <source>cleared the display name</source>
+        <translation>hat den Anzeigenamen gelöscht</translation>
+    </message>
+    <message>
+        <location filename="../src/models/messageeventmodel.cpp" line="226"/>
+        <source>changed the display name to %1</source>
+        <translation>hat den Anzeigename geändert zu</translation>
+    </message>
+    <message>
+        <location filename="../src/models/messageeventmodel.cpp" line="234"/>
+        <source>cleared the avatar</source>
+        <translation>hat den Avatar gelöscht</translation>
+    </message>
+    <message>
+        <location filename="../src/models/messageeventmodel.cpp" line="236"/>
+        <source>updated the avatar</source>
+        <translation>hat den Avatar aktualisiert</translation>
+    </message>
+    <message>
+        <location filename="../src/models/messageeventmodel.cpp" line="242"/>
+        <source>doesn&apos;t want %1 in the room anymore</source>
+        <translation>möchte, dass %1 den Raum verlässt</translation>
+    </message>
+    <message>
+        <location filename="../src/models/messageeventmodel.cpp" line="244"/>
+        <source>left the room</source>
+        <translation>hat den Raum verlassen</translation>
+    </message>
+    <message>
+        <location filename="../src/models/messageeventmodel.cpp" line="247"/>
+        <source>banned %1 from the room</source>
+        <translation>hat %1 aus dem Raum verbannt</translation>
+    </message>
+    <message>
+        <location filename="../src/models/messageeventmodel.cpp" line="249"/>
+        <source>self-banned from the room</source>
+        <translation>hat sich selbst aus dem Raum verbannt</translation>
+    </message>
+    <message>
+        <location filename="../src/models/messageeventmodel.cpp" line="251"/>
+        <source>knocked</source>
+        <translation>wurde aus dem Raum entfernt</translation>
+    </message>
+    <message>
+        <location filename="../src/models/messageeventmodel.cpp" line="262"/>
+        <source>set aliases to: %1</source>
+        <translation>ändert den Alias in: %1</translation>
+    </message>
+    <message>
+        <location filename="../src/models/messageeventmodel.cpp" line="267"/>
+        <source>set the room main alias to: %1</source>
+        <translation>ändert den Hauptalias des Raumes in: %1</translation>
+    </message>
+    <message>
+        <location filename="../src/models/messageeventmodel.cpp" line="272"/>
+        <source>set the room name to: %1</source>
+        <translation>ändert den Raumnamen in: %1</translation>
+    </message>
+    <message>
+        <location filename="../src/models/messageeventmodel.cpp" line="277"/>
+        <source>set the topic to: %1</source>
+        <translation>ändert das Thema in: %1</translation>
+    </message>
+    <message>
+        <location filename="../src/models/messageeventmodel.cpp" line="282"/>
+        <source>activated End-to-End Encryption (algorithm: %1)</source>
+        <translation>aktiviert End-zu-End-Verschlüsselung</translation>
+    </message>
+</context>
+<context>
+    <name>RoomsPage</name>
+    <message>
+        <location filename="../qml/pages/RoomsPage.qml" line="87"/>
+        <source>Rooms</source>
+        <translation>Räume</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/RoomsPage.qml" line="99"/>
+        <source>About Matriksi</source>
+        <translation>Über Matriksi</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/RoomsPage.qml" line="111"/>
+        <source>Logout</source>
+        <translation>Abmelden</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/RoomsPage.qml" line="112"/>
+        <source>Logging out</source>
+        <translation>Abmeldung</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/RoomsPage.qml" line="151"/>
+        <source>Join room...</source>
+        <translation>Betrete den Raum...</translation>
+    </message>
+</context>
+<context>
+    <name>SettingsPage</name>
+    <message>
+        <location filename="../qml/pages/SettingsPage.qml" line="16"/>
+        <source>Matriksi Settings</source>
+        <translation>Matriksi Einstellungen</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingsPage.qml" line="20"/>
+        <source>Fancy colors</source>
+        <translation>originelle Farben</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingsPage.qml" line="21"/>
+        <source>Use fancy colors on user names</source>
+        <translation>Benutzernamen in bunten Farben darstellen</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingsPage.qml" line="32"/>
+        <source>Dark background</source>
+        <translation>dunkler Hintergrund</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingsPage.qml" line="33"/>
+        <source>Use dark background on chat</source>
+        <translation>Nachrichtenfenster mit dunklem Hintergrund darstellen</translation>
+    </message>
+</context>
+<context>
+    <name>ThirdPartyLabel</name>
+    <message>
+        <location filename="../qml/components/about/ThirdPartyLabel.qml" line="42"/>
+        <source>License</source>
+        <translation>Lizenz</translation>
+    </message>
+    <message>
+        <location filename="../qml/components/about/ThirdPartyLabel.qml" line="51"/>
+        <source>Project Page</source>
+        <translation>Projekt Homepage</translation>
+    </message>
+</context>
+<context>
+    <name>ThirdPartyPage</name>
+    <message>
+        <location filename="../qml/pages/ThirdPartyPage.qml" line="26"/>
+        <source>Third Party</source>
+        <translation>Dritte</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ThirdPartyPage.qml" line="32"/>
+        <source>The LGPL-2.1 license</source>
+        <translation>Die LGPL-2.1</translation>
+    </message>
+</context>
+<context>
+    <name>TranslationsPage</name>
+    <message>
+        <location filename="../qml/pages/TranslationsPage.qml" line="17"/>
+        <source>Translators</source>
+        <translation>Übersetzer</translation>
+    </message>
+</context>
+</TS>
