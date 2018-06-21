57906e5c4352464efe132c0c343de677846cd31a
Alexey Andreyev <aa13q@ya.ru>
2018-06-21 21:21:00 +0300

    russian translation

---
+++ translations/harbour-matrix-ru.ts
@@ -0,0 +1,267 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ru_RU">
+<context>
+    <name>AboutPage</name>
+    <message>
+        <source>About Matriksi</source>
+        <translation>О программе</translation>
+    </message>
+    <message>
+        <source>An unofficial Matrix Client for SailfishOS</source>
+        <translation>Неофициальный клиент Matrix для SailfishOS</translation>
+    </message>
+    <message>
+        <source>Matriksi is an unofficial Matrix Client for SailfishOS and distributed under the GPLv3 license.</source>
+        <translation>Matriksi -- это неофициальный клиент Matrix для SailfishOS, распространяемый по лицензии GPLv3.</translation>
+    </message>
+    <message>
+        <source>Developers</source>
+        <translation>Разработчики</translation>
+    </message>
+    <message>
+        <source>Donate with Paypal</source>
+        <translation>Пожертвование через Paypal</translation>
+    </message>
+    <message>
+        <source>Translations</source>
+        <translation>Переводы</translation>
+    </message>
+</context>
+<context>
+    <name>ChatRoom</name>
+    <message>
+        <source>Say something...</source>
+        <translation>Напишите что-нибудь...</translation>
+    </message>
+</context>
+<context>
+    <name>ChatRoom2</name>
+    <message>
+        <source>Message @</source>
+        <translation>Сообщение @</translation>
+    </message>
+</context>
+<context>
+    <name>CoverPage</name>
+    <message>
+        <source>My Cover</source>
+        <translation>Моя обложка</translation>
+    </message>
+</context>
+<context>
+    <name>DevelopersPage</name>
+    <message>
+        <source>Developers</source>
+        <translation>Разработчики</translation>
+    </message>
+    <message>
+        <source>Previous developer</source>
+        <translation>Бывшие разработчики</translation>
+    </message>
+    <message>
+        <source>Matriksi developers</source>
+        <translation>Разработчкики клиента</translation>
+    </message>
+    <message>
+        <source>Contributors</source>
+        <translation>Помощники</translation>
+    </message>
+</context>
+<context>
+    <name>Login</name>
+    <message>
+        <source>Please wait...</source>
+        <translation>Подождите...</translation>
+    </message>
+    <message>
+        <source>Matriksi</source>
+        <translation>Matriksi</translation>
+    </message>
+    <message>
+        <source>User Name or Matrix ID:</source>
+        <translation>Имя пользователя или Matrix ID:</translation>
+    </message>
+    <message>
+        <source>username[:server][:port]</source>
+        <translation>username[:server][:port]</translation>
+    </message>
+    <message>
+        <source>Password:</source>
+        <translation>Пароль:</translation>
+    </message>
+    <message>
+        <source>Login</source>
+        <translation>Логин</translation>
+    </message>
+    <message>
+        <source>Create an Matrix account</source>
+        <translation>Создать аккаунт Matrix</translation>
+    </message>
+</context>
+<context>
+    <name>MessageEventModel</name>
+    <message>
+        <source>invited %1 to the room</source>
+        <translation>-- приглашение %1 в комнату</translation>
+    </message>
+    <message>
+        <source>joined the room</source>
+        <translation>-- вступление в комнату</translation>
+    </message>
+    <message>
+        <source>cleared the display name</source>
+        <translation>-- убрано отображаемое имя</translation>
+    </message>
+    <message>
+        <source>changed the display name to %1</source>
+        <translation>-- отображаемое имя изменено на %1</translation>
+    </message>
+    <message>
+        <source>doesn&apos;t want %1 in the room anymore</source>
+        <translation>-- больше не желает видеть пользователя %1 в комнате</translation>
+    </message>
+    <message>
+        <source>left the room</source>
+        <translation>-- выход из комнаты</translation>
+    </message>
+    <message>
+        <source>banned %1 from the room</source>
+        <translation>-- запрет на посещение комнаты пользователю %1</translation>
+    </message>
+    <message>
+        <source>self-banned from the room</source>
+        <translation>-- запрет на посещение комнаты самому себе</translation>
+    </message>
+    <message>
+        <source>knocked</source>
+        <translation>постучался</translation>
+    </message>
+    <message>
+        <source>set aliases to: %1</source>
+        <translation>-- установлен псевдоним: %1</translation>
+    </message>
+    <message>
+        <source>set the room main alias to: %1</source>
+        <translation>-- установлен основной псевдоним комнаты: %1</translation>
+    </message>
+    <message>
+        <source>set the room name to: %1</source>
+        <translation>-- установлено имя комнаты: %1</translation>
+    </message>
+    <message>
+        <source>set the topic to: %1</source>
+        <translation>-- установлена тема комнаты: %1</translation>
+    </message>
+    <message>
+        <source>activated End-to-End Encryption (algorithm: %1)</source>
+        <translation>-- включено сквозное шифрование (алгоритм: %1)</translation>
+    </message>
+    <message>
+        <source>cleared the avatar</source>
+        <translation>-- аватар убран</translation>
+    </message>
+    <message>
+        <source>updated the avatar</source>
+        <translation>-- аватар обновлен</translation>
+    </message>
+</context>
+<context>
+    <name>RoomsPage</name>
+    <message>
+        <source>Join room...</source>
+        <translation>Вступить в комнату...</translation>
+    </message>
+    <message>
+        <source>Rooms</source>
+        <translation>Комнаты</translation>
+    </message>
+    <message>
+        <source>About Matriksi</source>
+        <translation>О Matriksi</translation>
+    </message>
+    <message>
+        <source>Logout</source>
+        <translation>Выход</translation>
+    </message>
+    <message>
+        <source>Logging out</source>
+        <translation>Выходим</translation>
+    </message>
+    <message>
+        <source>Settings</source>
+        <translation>Настройки</translation>
+    </message>
+</context>
+<context>
+    <name>RoomsPage2</name>
+    <message>
+        <source>Rooms</source>
+        <translation>Комнаты</translation>
+    </message>
+    <message>
+        <source>About Matriksi</source>
+        <translation>О Matriksi</translation>
+    </message>
+    <message>
+        <source>Join room...</source>
+        <translation>Вступить в комнату...</translation>
+    </message>
+</context>
+<context>
+    <name>SettingsPage</name>
+    <message>
+        <source>Fancy colors</source>
+        <translation>Модные цвета</translation>
+    </message>
+    <message>
+        <source>Use fancy colors on user names</source>
+        <translation>Использовать разные цвета для пользователей без аватарок</translation>
+    </message>
+    <message>
+        <source>Dark background</source>
+        <translation>Темный фон</translation>
+    </message>
+    <message>
+        <source>Use dark background on chat</source>
+        <translation>Затемненный фон для страницы переписки</translation>
+    </message>
+    <message>
+        <source>Matriksi Settings</source>
+        <translation>Настройки Matriksi</translation>
+    </message>
+    <message>
+        <source>Settings</source>
+        <translation>Настройки</translation>
+    </message>
+</context>
+<context>
+    <name>ThirdPartyLabel</name>
+    <message>
+        <source>License</source>
+        <translation>Лицензия</translation>
+    </message>
+    <message>
+        <source>Project Page</source>
+        <translation>Страница проекта</translation>
+    </message>
+</context>
+<context>
+    <name>ThirdPartyPage</name>
+    <message>
+        <source>Third Party</source>
+        <translation>Сторонние компоненты</translation>
+    </message>
+    <message>
+        <source>The LGPL-2.1 license</source>
+        <translation>Лицензия LGPL-2.1</translation>
+    </message>
+</context>
+<context>
+    <name>TranslationsPage</name>
+    <message>
+        <source>Translators</source>
+        <translation>Переводчики</translation>
+    </message>
+</context>
+</TS>
